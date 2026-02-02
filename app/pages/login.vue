bde655f50e3fe24674a823084d97da4be8f7c4c8
wallcreation <lanhajames@gmail.com>
2026-02-02 23:43:39 +0100

    Add login page

---
+++ app/pages/login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="h-screen w-screen flex items-center justify-center">
+    <UAuthForm
+      title="O-Platy"
+      description="Connectez-vous"
+      :fields="fields"
+      :schema="schema"
+      :ui="{
+        title: 'text-4xl font-bold',
+        root: 'w-full max-w-sm mx-auto px-8'
+      }"
+      @submit="onSubmit"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({ layout: false })
+import type { AuthFormField, FormSubmitEvent } from '@nuxt/ui'
+import { z } from 'zod'
+
+const fields: AuthFormField[] = [
+  {
+    name: 'email',
+    type: 'email',
+    label: 'Email',
+    placeholder: 'votre@email.com',
+    required: true
+  },
+  {
+    name: 'password',
+    type: 'password',
+    label: 'Mot de passe',
+    placeholder: '••••••••',
+    required: true
+  }
+]
+
+const schema = z.object({
+  email: z.email('Email invalide'),
+  password: z.string().min(1, 'Mot de passe requis')
+})
+
+type Schema = z.output<typeof schema>
+
+function onSubmit(payload: FormSubmitEvent<Schema>) {
+  // payload.errors est déjà géré par le composant si validation échoue
+  // payload.data contient les données validées si succès
+  console.log('Connexion:', payload.data)
+}
+</script>
